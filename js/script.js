ca01080b6009222d1f002ea5327bcbdc5986c8d3
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-24 17:29:13 +0100

    refactor script.js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// $(function() {
+
+//   function getJoke() {
+//     $.ajax({
+//       url: 'http://api.icndb.com/jokes/random',
+//       method: 'GET',
+//       success: function() {
+//         $('#joke').text(res.value.joke);
+//       }
+//     });
+//   }
+
+//   const $button = $('#get-joke').click(function() {
+//     getJoke();
+//   });
+
+// });
+
+$(function() {
+
+  
+
+}); // END DOCUMENT READY
