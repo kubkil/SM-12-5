c5c98e55af1ec2ff72ca3871160fa94c3129f031
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-24 20:28:13 +0100

    finish random quote machine

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
+$(function() {
+
+  // linki - podwójne cytaty? If you are constructing html from strings, either use " instead of ' for attribute quotes, or add an extra layer of encoding (' can be encoded as %27).
+  const prefix = "https://cors-anywhere.herokuapp.com/";
+  const tweetLink = "https://twitter.com/intent/tweet?text=";
+  const quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+  // co wskakuje jako argument 'input'?
+  function createTweet(input) {
+    let data = input[0];
+    // data.content ?
+    // data.content - kod HTML paragrafu; tworzymy element HTML z tego kodu, za pomocą text() wyciągamy zawartość tekstową i ucinamy spacje (trim())
+    const quoteText = $(data.content).text().trim();
+    const quoteAuthor = data.title;
+    const tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
+    // .length - the number of elements currently matched. If (true) czyli jeśli jest 0, zamienia na true i warunek jest spełniony
+    if (!quoteAuthor.length) {
+      quoteAuthor = 'Unknown author';
+    }
+
+    if (tweetText.length > 140) {
+      getQuote();
+    } else {
+      // encodeURIComponent ?
+      const tweet = tweetLink + encodeURIComponent(tweetText);
+      // znajdź element o klasie quote i wsadź quoteText
+      $('.quote').text(quoteText);
+      $('.author').text('Author: ' + quoteAuthor);
+      // w atrybut href wstawia 'tweet'
+      $('.tweet').attr('href', tweet);
+    }
+   }
+
+  function getQuote() {
+    // pierwszy parametr - link do API, drugi - wykonywana funkcja
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
+  }
+
+  $('.trigger').click(function () {
+    getQuote();
+  });
+  getQuote();
+
+}); // END DOCUMENT READY
+
 // $(function() {
 
 //   function getJoke() {
@@ -15,9 +61,3 @@
 //   });
 
 // });
-
-$(function() {
-
-  
-
-}); // END DOCUMENT READY
